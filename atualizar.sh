123bd0b71af2ab00b4fa57609116caa0dcb5400c
Victor Torres <victor68346@estudante.ifb.edu.br>
2026-02-28 09:33:46 -0300

    Teste Atualização Automatica

---
+++ atualizar.sh
@@ -0,0 +1,5 @@
+git add .
+read mensagem
+git commit -m "$mensagem"
+git branch -M main
+git push -u origin main
---
+++ criar.sh
@@ -0,0 +1,7 @@
+echo "# Minha-Primeira-API" >> README.md
+git init
+git add README.md
+git commit -m "Primeiro Commit"
+git branch -M main
+git remote add origin https://github.com/Bluepen-Oficial/Minha-Primeira-API.git
+git push -u origin main

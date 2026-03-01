c5c2db9bcb9f6288c64be1a161234189df939327
Victor Torres <victor68346@estudante.ifb.edu.br>
2026-03-01 13:28:48 -0300

    adicao da API CEP

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 const numeroCEP = document.querySelector('#numCep');
-const resultado = document.querySelector('#reultado');
-const regex = /\d{5}-?\d{3}/g;
+const resultado = document.querySelector('#resultado');
+const botao = document.querySelector('#btnBuscar');
+const regex = /^\d{5}-?\d{3}$/;
 
-function buscarCEP(CEP) {
-    if (numeroCEP == " " || regex.test(CEP)){
-        resultado.innerHTML = "Digite um CEP válido!"
-    }
+botao.addEventListener('click', () => { buscarCEP() });
+
+function buscarCEP() {
+    const validacaoCEP = numeroCEP.value.replace(/[\s\-]/g, '').trim();
+    console.log("A", validacaoCEP, validarExpressao(validacaoCEP));
+
+    if (validacaoCEP === "" || validarExpressao(validacaoCEP) === false) {
+        console.log("B")
+        resultado.innerHTML = "Digite um CEP válido!";
+        return
+    } 
+
+    buscarEndereco(validacaoCEP);
+}
+
+function validarExpressao(valor) {
+    return regex.test(valor);
+}
+
+function buscarEndereco(numCEP) {
+    fetch(`https://cep.awesomeapi.com.br/json/${numCEP}`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            const endereco = dados;
+            console.log(endereco);
+            mostrarEndereco(endereco);
+        })
+        .catch (() => {
+            resultado.innerHTML = "Endereço não encontrado!";
+        });
+}
+
+function mostrarEndereco(busca) {
+    console.log("E");
+    resultado.innerHTML =  JSON.stringify(busca);
 }
